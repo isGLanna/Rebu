6955f49e6033f54ffa84a3af6a848d27987e7998
Giordano Lanna <giordanoal2003@gmail.com>
2026-06-12 15:07:17 -0300

    uns scripts pra facilitar execução que esqueci de subir

--- infra/scripts/01-build-cluster.sh
+++ infra/scripts/01-build-cluster.sh
@@ -8,3 +8,8 @@ kind load docker-image rebu-backend:v1 --name rebu-cluster
 
 kind apply -f k8s/ingress.yaml
 kind apply -f k8s/auto-scaler.yaml
+
+# Gera a imagem do back
+# Cria o cluster e passa as configurações para o kubectl
+# Carrega a imagem do backend para o cluster
+# Aplica as configurações de ingress e auto-scaler no cluster
